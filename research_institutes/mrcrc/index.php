4cbd7bb64215c97f347d5af1f0c4acdedbc67d48
uokedu <smnoman@uok.edu.pk>
2026-09-14 13:17:09 +0500

    Remove strong tag from incharge name

--- research_institutes/mrcrc/index.php
+++ research_institutes/mrcrc/index.php
@@ -61,7 +61,7 @@
 </div>
 </div>
 <div class="right_course">
-  <h2>Incharge: <strong>Ms. Wafra Matant Zaheen</strong></h2>
+  <h2>Incharge: Ms. Wafra Matant Zaheen</h2>
   <table width="97%" border="0">
     <tr>
       <td width="8%">&nbsp;</td>
